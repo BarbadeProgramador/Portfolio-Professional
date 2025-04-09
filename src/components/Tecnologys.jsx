5f748e59213e3b4d3e1002f74edb96f6444ac7da
BarbadeProgramador <sebastianvasquezz200@gmail.com>
2025-04-09 05:04:56 -0500

    solution bug

--- src/components/Tecnologys.jsx
+++ src/components/Tecnologys.jsx
@@ -1,8 +1,8 @@
 export function Card_Ability({ data }) {
 
     return (
-        <div className="content-tecnology">
-            <img src={data.img} alt="tecnologia" loading="lazy" />
+        <div className="content-tecnology" >
+            <img src={data.img} alt="tecnologia" loading="lazy" data-aos="fade-up" />
             <h3>{data.name}</h3>
         </div>
     );
